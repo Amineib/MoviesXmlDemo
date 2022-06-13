3c4b96f3bd51654bb4820bf0698fd9887ade7859
naar0x <mohamedamine.ib@gmail.com>
2022-06-13 12:30:09 +0200

    API key secrets gradle plugin

--- app/src/main/java/com/naar/myapplication/util/ApiKeyInterceptor.kt
+++ app/src/main/java/com/naar/myapplication/util/ApiKeyInterceptor.kt
@@ -1,19 +1,21 @@
 package com.naar.myapplication.util
 
-import com.naar.myapplication.R
+import com.naar.myapplication.BuildConfig
 import okhttp3.Interceptor
 import okhttp3.Response
 
 private const val API_KEY = "a48952ed90a177f26bb620a6fca58be9"
+//private const val API_KEY = BuildConfig.apiKey
 private const val API_KEY_NAME = "api_key"
 
 class ApiKeyInterceptor : Interceptor {
     override fun intercept(chain: Interceptor.Chain): Response {
-        var original = chain.request()
-        val url = original.url.newBuilder()
-            .addQueryParameter(API_KEY_NAME, API_KEY)
-            .build()
-        original = original.newBuilder().url(url).build()
-        return chain.proceed(original)
+
+            var original = chain.request()
+            val url = original.url.newBuilder()
+                .addQueryParameter(API_KEY_NAME, API_KEY)
+                .build()
+            original = original.newBuilder().url(url).build()
+            return chain.proceed(original)
     }
 }
